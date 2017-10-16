4c5aeb25bdf632ab06c2dd3dae99e42fa5a868d3
Maria <maria.saavedra@luminartech.com>
2017-10-16 17:29:38 -0400

    styling connection issues

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var session    = require('express-session');
 var bodyParser = require('body-parser');
 var env = require('dotenv').load();
 var exphbs = require('express-handlebars');
+var path = require('path');
 
 var PORT = process.env.PORT || 5000;
 
@@ -37,6 +38,9 @@ var authRoute = require('./app/routes/auth.js')(app,passport);
 
 //load passport strategies
 require('./app/config/passport/passport.js')(passport, models.user);
+
+//static file
+app.use(express.static(__dirname + '/views'));
 
 //For Handlebars
 app.set('views', './app/views')
