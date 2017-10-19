8d5dbd2b4c96da95f1825aeae02fac54fe787bec
Maria <maria.saavedra@luminartech.com>
2017-10-19 12:57:15 -0400

    search js issues

---
+++ public/js/search.js
@@ -0,0 +1,21 @@
+
+
+// $("#filter").keyup(function () {
+// 	// body...
+
+// 	//retrieve this input field text 
+// 	var filter = $(this).val();
+
+// 	//loop trough the site info
+
+// 	$("h p li").each(function(){
+
+// 		//If the tags do not contain the text phrase fade it out
+// 		if($(this).text().search(new RegExp(filter, "i")) <0) {
+// 			$(this).fadeOut();
+// 		}else {
+// 			//show the text item if the phrase matches
+// 			$(this).show();
+// 		}
+// 	});
+// });
